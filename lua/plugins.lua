6da1dee0ba0a47b279267703891feae5ea477ee5
QuiGon189 <leshka189@gmail.com>
2025-02-10 16:23:38 +0300

    add command to set font

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,10 +1,15 @@
+-- команды для установки шрифта
+-- mkdir -p ~/.local/share/fonts
+-- cd ~/.local/share/fonts
+-- curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/DroidSansMNerdFont-Regular.otf
+------------------------------------------
 local fn = vim.fn
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 print("install_path: " .. install_path)
 if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    download_result = fn.system({'ls', '-l', install_path})
-    print("download_result: " .. download_result)
+	packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+	download_result = fn.system({'ls', '-l', install_path})
+	print("download_result: " .. download_result)
 end
 
 vim.cmd [[packadd packer.nvim]]
@@ -14,35 +19,35 @@ return require('packer').startup(function(use)
 	-- Информационная строка
 	use "nvim-tree/nvim-web-devicons"
 	use { 'nvim-lualine/lualine.nvim',
-	    requires = {'nvim-tree/nvim-web-devicons', opt = true},
-			config = function()
-				require('lualine').setup()
-			end, 
+	requires = {'nvim-tree/nvim-web-devicons', opt = true},
+	config = function()
+		require('lualine').setup()
+	end,
+}
+use({
+	'rose-pine/neovim',
+	as = 'rose-pine',
+	config = function()
+		vim.cmd('colorscheme rose-pine-moon')
+	end
+})
+
+-- МОДУЛИ РЕДАКТОРА
+-- Табы с кладками
+use {'akinsho/bufferline.nvim',
+requires = 'nvim-tree/nvim-web-devicons',
+config = function()
+	require("bufferline").setup {}
+end,
 		}
-  use({
-		'rose-pine/neovim',
-		as = 'rose-pine',
+
+		-- Файловй менеджер
+		use { 'nvim-tree/nvim-tree.lua',
+		requires = 'nvim-tree/nvim-web-devicons',
 		config = function()
-			vim.cmd('colorscheme rose-pine-moon')
-		end
-	})
-
-	-- МОДУЛИ РЕДАКТОРА
-	-- Табы с кладками
-	use {'akinsho/bufferline.nvim',
-	    requires = 'nvim-tree/nvim-web-devicons',
-			config = function()
-				require("bufferline").setup {}
-			end,
-		}
-
-  -- Файловй менеджер
-	use { 'nvim-tree/nvim-tree.lua',
-	    requires = 'nvim-tree/nvim-web-devicons',
-			config = function()
-				require 'nvim-tree'.setup {}
-			end,
-		}
+			require 'nvim-tree'.setup {}
+		end,
+	}
 
 	-- popup windows
 	use 'nvim-lua/popup.nvim'
@@ -93,6 +98,6 @@ return require('packer').startup(function(use)
 	use 'skanehira/translate.vim'
 
 	if packer_bootstrap then
-	    require('packer').sync()
+		require('packer').sync()
 	end
 end)
